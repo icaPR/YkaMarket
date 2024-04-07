f17320cbbb746076b86ba681f71d58491fb4d997
Icaro Ramos <polosnramos@hotmail.com>
2024-04-06 22:05:28 -0300

    feat: Create theme file and its typings

---
+++ ykamarket/src/theme/index.ts
@@ -0,0 +1,28 @@
+export default {
+  COLORS: {
+    gray_1: "#1A1B1B",
+    gray_2: "#3E3A40",
+    gray_3: "#5F5B62",
+    gray_4: "#9F9BA1",
+    gray_5: "#D9D8DA",
+    gray_6: "#EDECEE",
+    gray_7: "#F7F7F8",
+
+    blue: "#364D9D",
+    blue_light: "#647AC7",
+    red_light: "#EE7979",
+  },
+  FONT_FAMILY: {
+    REGULAR: "Karla_400Regular",
+    BOLD: "Karla_700Bold",
+    EXTRABOLD: "Karla_800ExtraBold",
+  },
+  FONT_SIZE: {
+    XS: 12,
+    SM: 14,
+    MD: 16,
+    LG: 18,
+    XL: 20,
+    XXL: 24,
+  },
+};
